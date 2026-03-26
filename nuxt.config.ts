257c50b6e461d1230bacc988ccdd892276688dea
nurmuhammadnizomov <nurmuhammadnizomov@gmail.com>
2026-03-26 22:22:41 +0500

    fix: gsap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -35,6 +35,11 @@ export default defineNuxtConfig({
       ],
     },
   },
+  vite: {
+    optimizeDeps: {
+      include: ['gsap'],
+    },
+  },
   colorMode: {
     preference: 'system',
     fallback: 'dark',
